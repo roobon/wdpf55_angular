2ae4c468981c99a349ab84eb6be0917a2185bc37
Roobon <roobon@gmail.com>
2023-10-09 11:21:57 +0600

    Todo list and todo items

--- todo1/src/app/app.component.ts
+++ todo1/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { TodoList } from "./todoList";
+import { TodoItem } from "./todoItem";
+
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  private list = new TodoList("Bob", [
+    new TodoItem("Go for run"),
+    new TodoItem("Get flowers"),
+    new TodoItem("Collect tickets"),
+    new TodoItem("Go to Bazar"),
+    ]);
+    get username(): string {
+    return this.list.user;
+    }
+    get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length;
+    }
 }
